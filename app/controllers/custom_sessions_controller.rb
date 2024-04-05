7fc03da8b2c65021cb151f3fb216ee87d1b0f28e
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-04-05 22:37:41 +0800

    Create condition route for admin and trader

---
+++ app/controllers/custom_sessions_controller.rb
@@ -0,0 +1,22 @@
+class CustomSessionsController < Devise::SessionsController
+    def after_sign_in_path_for(resource)
+        if resource.admin?
+            admins_path
+        else
+            traders_path
+        end
+    end
+
+    # before_action :authenticate_admin!
+
+    # def after_sign_in_path_for
+    #   @admins = User.where(admin: true)
+    # end
+  
+    # private
+  
+    # def authenticate_admin!
+    #   redirect_to traders_path, alert: "You are not authorized to access this page." unless current_user.admin?
+    # end
+end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {sessions: 'custom_sessions'}
+
+  authenticated :user do
+    root to: 'custom_sessions#after_sign_in_path_for', as: :authenticated_root
+  end
 
   devise_scope :user do
-    authenticated :user do
-      root 'home#index', as: :authenticated_root
-    end
-  
     unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
+      root to: 'devise/sessions#new', as: :unauthenticated_root
     end
   end
+
+  resources :admins 
+  resources :traders 
+
+  #this route is only for creating initial admin
+  patch 'grant_admin', to: 'admins#grant_admin', as: 'grant_admin_admin'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -17,6 +24,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "users#custom_login_register"
   
 end
