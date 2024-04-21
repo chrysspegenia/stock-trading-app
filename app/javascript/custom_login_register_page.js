f8202537f56c3a322ad20ebda58d2854f67be973
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-04-21 21:41:01 +0800

    Modify addEventListener DomContentLoad

--- app/javascript/custom_login_register_page.js
+++ app/javascript/custom_login_register_page.js
@@ -1,13 +1,19 @@
 document.addEventListener("turbo:load", () => {
-  const wrapper = document.querySelector(".main-custom-wrapper");
+  document.addEventListener("DOMContentLoaded", () => {
+    // Wait for DOM content to load
+    const wrapper = document.querySelector(".main-custom-wrapper");
 
-  wrapper.addEventListener("click", (event) => {
-    const container = wrapper.querySelector(".main-custom-container");
+    if (wrapper) {
+      // Check if wrapper exists before adding event listener
+      wrapper.addEventListener("click", (event) => {
+        const container = wrapper.querySelector(".main-custom-container");
 
-    if (event.target.classList.contains("register-link")) {
-      container.classList.add("active");
-    } else if (event.target.classList.contains("login-link")) {
-      container.classList.remove("active");
+        if (event.target.classList.contains("register-link")) {
+          container.classList.add("active");
+        } else if (event.target.classList.contains("login-link")) {
+          container.classList.remove("active");
+        }
+      });
     }
   });
 });
