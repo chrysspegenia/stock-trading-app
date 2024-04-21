2fe585b771cadba7a74cef59f0dc482c7f3c0ab0
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-04-21 22:53:24 +0800

    Fix issue where eventListener error on login

--- app/javascript/custom_login_register_page.js
+++ app/javascript/custom_login_register_page.js
@@ -1,19 +1,23 @@
 document.addEventListener("turbo:load", () => {
-  document.addEventListener("DOMContentLoaded", () => {
-    // Wait for DOM content to load
-    const wrapper = document.querySelector(".main-custom-wrapper");
+  const wrapper = document.querySelector(".main-custom-wrapper");
 
-    if (wrapper) {
-      // Check if wrapper exists before adding event listener
-      wrapper.addEventListener("click", (event) => {
-        const container = wrapper.querySelector(".main-custom-container");
+  if (wrapper) {
+    // Check if wrapper exists
+    const handleWrapperClick = (event) => {
+      const container = wrapper.querySelector(".main-custom-container");
 
-        if (event.target.classList.contains("register-link")) {
-          container.classList.add("active");
-        } else if (event.target.classList.contains("login-link")) {
-          container.classList.remove("active");
-        }
-      });
-    }
-  });
+      if (event.target.classList.contains("register-link")) {
+        container.classList.add("active");
+      } else if (event.target.classList.contains("login-link")) {
+        container.classList.remove("active");
+      }
+    };
+
+    wrapper.addEventListener("click", handleWrapperClick);
+
+    // Remove event listener when leaving page
+    document.addEventListener("turbo:before-cache", () => {
+      wrapper.removeEventListener("click", handleWrapperClick);
+    });
+  }
 });
