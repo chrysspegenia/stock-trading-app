f347acbbb6572a7a3b1f0df88a9306649ae0de38
chad <chadayop@gmail.com>
2024-04-29 00:36:13 +0800

    System test: balance

--- spec/system/trader_buy_sell_spec.rb
+++ spec/system/trader_buy_sell_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Trading', type: :system do
   end
 
   it 'allows buying a stock if balance is sufficient' do
-    Capybara.default_max_wait_time = 30
+    # Capybara.default_max_wait_time = 30
     visit buy_new_trader_path(company.symbol)
 
     fill_in :quantity, with: 5
@@ -20,7 +20,7 @@ RSpec.describe 'Trading', type: :system do
   end
 
   it 'does not allow buying a stock if balance is insufficient' do
-    Capybara.default_max_wait_time = 30
+    # Capybara.default_max_wait_time = 30
     trader.update(balance: 8.00)
 
     visit buy_new_trader_path(company.symbol)
@@ -32,9 +32,7 @@ RSpec.describe 'Trading', type: :system do
   end
 
   it 'allows selling a stock if stock quantity is not zero' do
-    Capybara.default_max_wait_time = 30
     company.update(shares: 10)
-
     visit sell_new_trader_path(company.symbol)
 
     fill_in :quantity, with: 5
@@ -44,9 +42,7 @@ RSpec.describe 'Trading', type: :system do
   end
 
   it 'does not allow selling a stock if stock quantity is zero' do
-    Capybara.default_max_wait_time = 30
     company.update(shares: 0)
-
     visit sell_new_trader_path(company.symbol)
 
     fill_in 'quantity', with: 5
