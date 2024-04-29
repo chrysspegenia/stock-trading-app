b40857410e2bb81da43796ae298b97d8ee3a9367
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-04-29 10:47:07 +0800

    Include rails charts dependency

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,5 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 import "./custom_login_register_page.js";
-import "echarts"
-import "echarts/theme/dark"
+import "echarts";
+import "echarts/theme/dark";
