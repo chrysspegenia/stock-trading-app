9e188ee2a8245f5b972169b36fd2b37c2311ca0d
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-04-29 17:28:39 +0800

    Replace secret key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,8 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  config.secret_key_base = Rails.application.credentials.secret_key_base
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
