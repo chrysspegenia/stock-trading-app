17ea6e538b8dd5b1de48ccd9760a448741da6ef0
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-04-30 00:46:19 +0800

    Remove custom login page import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "./custom_login_register_page.js";
 import "echarts";
 import "echarts/theme/dark";
 
