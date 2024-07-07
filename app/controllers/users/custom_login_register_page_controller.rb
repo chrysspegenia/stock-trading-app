367d6949a2c4ad224da8e7f77a2e5e5b12b953c9
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-07-07 18:17:32 +0800

    Add devise resources method

--- app/controllers/users/custom_login_register_page_controller.rb
+++ app/controllers/users/custom_login_register_page_controller.rb
@@ -1,6 +1,13 @@
 class Users::CustomLoginRegisterPageController < ApplicationController
     skip_before_action :authenticate_user!
+    before_action :set_devise_resource
 
     def index
+    end
+
+    def set_devise_resource
+        @resource = User.new
+        @resource_name = :user
+        @devise_mapping = Devise.mappings[:user]
     end
 end
